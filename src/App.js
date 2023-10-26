8c13d9269e12ef923377c16f56ac69ceb6fe6e9a
Lucas Carlos <lucas2carlos@gmail.com>
2023-10-26 12:58:53 -0400

    adding Idea Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import IdeaGenerationPage from './pages/IdeaGeneratorPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="">
+      <div className='h-screen bg-slate-900 font-sans overflow-hidden relative'>
+        <div className='flex flex-col flex-wrap'>
+          <IdeaGenerationPage />
+        </div>
+      </div>
     </div>
   );
 }
