50e4d61e25368369e9eaa80ccb590edc389ef8f5
Lucas Carlos <lucas2carlos@gmail.com>
2023-10-27 13:48:00 -0400

    adding styles

--- src/pages/IdeaGeneratorPage.jsx
+++ src/pages/IdeaGeneratorPage.jsx
@@ -4,12 +4,20 @@ export default function IdeaGenerationPage () {
     const [targetAudience, setTargetAudience] = useState("")
     const [interest, setInterest] = useState("")
     return(
-        <div className="w-full mx-4 mx-8 text-center">
-            <div>Create Your Micro SaaS Idea</div>
-            <input className="bg-gray-400 text-black" type="text" placeholder="" onChange={(e) => setTargetAudience(e.target.value)} value={targetAudience} />
-            <input className="bg-gray-400 text-black" type="text" placeholder="" onChange={(e) => setInterest(e.target.value)} value={interest} />
-            <div>
-                Button
+        <div>
+            <div className="w-full my-20 flex flex-col gap-4 max-w-lg mx-auto">
+                <div className="text-3xl text-center font-bold">Create Your Micro SaaS Idea</div>
+                <div className="flex flex-col">
+                    <label htmlFor="targetAudience" className="font-semibold my-2">Target Audience</label>
+                    <input className="bg-gray-400 text-black placeholder-black" id="targetAudience" type="text" placeholder="Shopify Merchants" onChange={(e) => setTargetAudience(e.target.value)} value={targetAudience} />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="interest" className="font-semibold my-2">Interest</label>
+                    <input className="bg-gray-400 text-black placeholder-black" id="interest" type="text" placeholder="AI SEO" onChange={(e) => setInterest(e.target.value)} value={interest} />
+                </div>
+                <div className="py-2 px-2 items-center font-semi-bold w-full text-md bg-pink-800 hover:bg-pink-600 cursor-pointer rounded-md text-center">
+                    Generate Description
+                </div>
             </div>
         </div>
     )
