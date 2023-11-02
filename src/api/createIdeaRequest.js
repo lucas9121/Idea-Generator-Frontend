979e1b97356be606eb8436706fdf94a4565679a7
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-11-01 22:37:48 -0400

    updating function

--- src/api/createIdeaRequest.js
+++ src/api/createIdeaRequest.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 
 export default async function createIdeaRequest(data) {
-    await axios({
-        method: 'POST',
-        url: `${process.env.REACT_APP_SERVER_URL}/api/v1/createIdeaGeneration`,
-        data,
-    })
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: `${process.env.REACT_APP_SERVER_URL}/api/v1/createIdeaGeneration`,
+            data,
+        })
+        
+        if(res && res.data && res.data.data){
+            return res.data.data
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
 }
