e7646aba148bec330c9615dced77afff11e1cf69
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-11-05 03:03:32 -0500

    adding loading screen

--- src/pages/IdeaGeneratorPage.jsx
+++ src/pages/IdeaGeneratorPage.jsx
@@ -7,10 +7,11 @@ export default function IdeaGenerationPage () {
     const [targetAudience, setTargetAudience] = useState("")
     const [interest, setInterest] = useState("")
     const [genereatedIdeasFromAPI, setGenereatedIdeasFromAPI] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleSubmit = async(evt) => {
         evt.preventDefault();
-        console.log('clicked!')
+        setIsLoading(true)
         try {
             const response = await createIdeaRequest({ targetAudience, interest });
         
@@ -19,11 +20,14 @@ export default function IdeaGenerationPage () {
               //removing empty spaces
               const nonEmptyIdeas = openAIFinalResponse.filter((idea) => idea.trim() !== "");
               setGenereatedIdeasFromAPI(nonEmptyIdeas);
+              setIsLoading(false)
             } else {
               console.error("Response or openAIFinalResponse is undefined or missing in the API response.");
+              setIsLoading(false)
             }
           } catch (error) {
             console.error("An error occurred while making the API request:", error);
+            setIsLoading(false)
           }
     }
 
@@ -40,15 +44,18 @@ export default function IdeaGenerationPage () {
                     <label htmlFor="interest" className="font-semibold my-2">Interest</label>
                     <input className="bg-gray-400 text-black placeholder-black" id="interest" type="text" placeholder="AI SEO" onChange={(e) => setInterest(e.target.value)} value={interest} />
                 </div>
-                <div className="py-2 px-2 items-center font-semi-bold w-full text-md bg-pink-800 hover:bg-pink-600 cursor-pointer rounded-md text-center" onClick={(e) => handleSubmit(e)}>
+                <button className={isLoading ? "py-2 px-2 items-center font-semi-bold w-full text-md bg-pink-800 rounded-md text-center disabled:opacity-75" : "py-2 px-2 items-center font-semi-bold w-full text-md bg-pink-800 rounded-md text-center hover:bg-pink-600 cursor-pointer"} onClick={(e) => handleSubmit(e)} disabled={isLoading}>
                     Generate Ideas
-                </div>
+                </button>
             </div>
-            {genereatedIdeasFromAPI.length && (
+            { isLoading ? (
+                <div className="spinner-container">
+                    <div className="loading-spinner"></div>
+              </div>
+            ) : genereatedIdeasFromAPI.length && (
                 <div className="h-3/6 w-full max-w-lg mx-auto overflow-auto">
                     <div className="text-3xl">Your Ideas</div>
                     {genereatedIdeasFromAPI.map((idea, idx) => {
-                        console.log(idx)
                         const [title, description] = idea.split(':')     
                         return (
                             <div key={idx} className="my-4 text-center border border-white rounded py-2 px-4">
